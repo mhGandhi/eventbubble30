d839ff74d57bcac509e779c264c396a477c9df51
mhGandhi <adrian.akipi@web.de>
2025-11-15 00:55:01 +0100

    Initial commit

---
+++ src/main/java/com/lennadi/eventbubble30/Eventbubble30Application.java
@@ -0,0 +1,13 @@
+package com.lennadi.eventbubble30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eventbubble30Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eventbubble30Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lennadi/eventbubble30/Eventbubble30ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lennadi.eventbubble30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eventbubble30ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
