3541b448eb7ca2551a20bea35b4495abb3325917
mhGandhi <adrian.akipi@web.de>
2025-11-15 19:26:06 +0100

    kompletteres entity für Veranstaltungen

--- src/main/java/com/lennadi/eventbubble30/entities/Veranstaltung.java
+++ src/main/java/com/lennadi/eventbubble30/entities/Veranstaltung.java
@@ -1,26 +1,23 @@
 package com.lennadi.eventbubble30.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
 
 @Entity
+@Getter @Setter
 public class Veranstaltung {
     @Id
-    @Getter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
     private Long id;
 
-    @Getter
-    @Setter//todo only set once
     private Instant creationDate;
 
-    @Getter
-    @Setter
     private Instant termin;
+
+    private String title;
+    private String description;
 }
