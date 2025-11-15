60f2eb756a7bad3615f28d6809be10251903ab75
mhGandhi <adrian.akipi@web.de>
2025-11-15 21:04:18 +0100

    convert von Benutzer intern

--- src/main/java/com/lennadi/eventbubble30/dto/BenutzerDTO.java
+++ src/main/java/com/lennadi/eventbubble30/dto/BenutzerDTO.java
@@ -1,7 +1,13 @@
 package com.lennadi.eventbubble30.dto;
 
+import com.lennadi.eventbubble30.entities.Benutzer;
+
 public record BenutzerDTO(
         Long id,
         String email,
         String username
-) {}
+) {
+    public static BenutzerDTO fromBenutzer(Benutzer benutzer) {
+        return new BenutzerDTO(benutzer.getId(), benutzer.getEmail(), benutzer.getUsername());
+    }
+}
