de13476a29706a2123fafd9b7ec6c5b1f6218932
mhGandhi <adrian.akipi@web.de>
2025-11-15 20:21:37 +0100

    bischen versuchen docs zu generieren

--- src/test/java/com/lennadi/eventbubble30/integration/BenutzerIntegrationTest.java
+++ src/test/java/com/lennadi/eventbubble30/integration/BenutzerIntegrationTest.java
@@ -2,16 +2,19 @@ package com.lennadi.eventbubble30.integration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)   // <<< Security deaktiviert
+@AutoConfigureRestDocs(outputDir = "build/generated-snippets")
 class BenutzerIntegrationTest {
 
     @Autowired
@@ -27,9 +30,12 @@ class BenutzerIntegrationTest {
         }
         """;
 
-        mvc.perform(post("/api/user/create")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(status().isCreated());
+        mvc.perform(
+                        post("/api/user/create")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(json)
+                )
+                .andExpect(status().isCreated())
+                .andDo(document("create-user")); // <<< SNIPPET ERZEUGEN!
     }
 }
