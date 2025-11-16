5690a46eca11cf1f50b6cda59d63ab8f7cefc895
mhGandhi <adrian.akipi@web.de>
2025-11-16 19:24:18 +0100

    Mehr Todo. (ist peak Inspo)

--- src/main/java/com/lennadi/eventbubble30/Eventbubble30Application.java
+++ src/main/java/com/lennadi/eventbubble30/Eventbubble30Application.java
@@ -10,4 +10,19 @@ public class Eventbubble30Application {
         SpringApplication.run(Eventbubble30Application.class, args);
     }
 
+    /*todo
+    Inspo:
+        Audit-Log: Neues Entity das speichert WER WANN WAS(+Details) MIT WAS gemacht hat.
+        Bekommt ein Repo und einen Manager mit einer .log([...]) Methode
+
+        Ortschaften: Orte von Veranstaltungen irgendwie Speichern. Dazu vlt OpenStreetmapAPI oder so
+
+        iCal Export: api/events/{id}/export.ics -> Endpunkt für Kalenderexport
+
+        api/admin endpoint: das bisschen von dem regulären Betrieb trennen.
+        Extra Endpunkte wie active-users, user-count und Audit-Log Abruf etc. -> später im Frontend Admin Dashboard
+
+        Email Verifikation: an bestehende, unimplementierte Endpunkte in AuthController anknüpfen.
+        Email Schicken + Token Speichern (mit Ablauf)->Token Abgleichen und Löschen (+pw ändern)
+     */
 }
