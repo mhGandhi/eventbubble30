c0f286a0dc6d105ec85671996e454cd648ce8fdb
mhGandhi <adrian.akipi@web.de>
2025-11-26 03:24:43 +0100

    mehr todo

--- src/main/java/com/lennadi/eventbubble30/Eventbubble30Application.java
+++ src/main/java/com/lennadi/eventbubble30/Eventbubble30Application.java
@@ -23,6 +23,21 @@ public class Eventbubble30Application {
         Extra Endpunkte wie active-users, user-count und Audit-Log Abruf etc. -> später im Frontend Admin Dashboard
 
         Email Verifikation: an bestehende, unimplementierte Endpunkte in AuthController anknüpfen.
-        Email Schicken + Token Speichern (mit Ablauf)->Token Abgleichen und Löschen (+pw ändern)
+        Email Schicken + Token Speichern (mit Ablauf @Scheduled)->Token Abgleichen und Löschen (wenn pw geändert)
+
+        Profile: Entity getrennt von Benutzer, verbunden mit 1zu1 Beziehung, separate Endpunkte aber dennoch über
+        Id des Users identifiziert -> Bio, Name, Events
+
+        Medien: Object Storage, URLs in Entities speichern, ans Frontend nur temporär signierte URLs
+        -> Avatare, Bilder für Veranstaltungen, Banner etc
+
+        Soft-Delete: Gelöschte Entitäten mit "boolean: deleted" o.Ä. flaggen + timestamp setzen um Wiederherstellung
+        zu erlauben. Nach bestimmter Zeit dann löschen.
+
+        Rate-Limiting: login, refresh und signup pro IP + pro Username limitieren
+
+        Moderation: Nutzer suspendieren, bannen, löschen. Beiträge melden. Moderator Ansicht für gemeldete Beiträge
+
+        Event-Erinnerungen, Tags+Kategorien für Events, einfache Suche +Filter, Privatsphäreeinstellungen Profile+Events, Webhooks, Session Overview, MFA, Cache Layer, Telemetrie
      */
 }
