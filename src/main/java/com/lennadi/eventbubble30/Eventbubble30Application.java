fabc0fa0f14af02e83451a497c9bcdba7b67aac5
mhGandhi <adrian.akipi@web.de>
2025-11-28 02:26:54 +0100

    todo updates

--- src/main/java/com/lennadi/eventbubble30/Eventbubble30Application.java
+++ src/main/java/com/lennadi/eventbubble30/Eventbubble30Application.java
@@ -17,24 +17,17 @@ public class Eventbubble30Application {
     Inspo:
         Ortschaften: Orte von Veranstaltungen irgendwie Speichern. Dazu vlt OpenStreetmapAPI oder so, (Nominatim?)
 
-        -> später im Frontend Admin Dashboard
-
         @PreAuthorize 401: PreAuthorize gibt stets 403 zurück, auch wenn eigentlich 401 angebracht wäre :/
 
-        vlt kein Login erlauben wenn schon drin? (eig unnötig)
-
         JacksonEntitySerializationBlocker: Dass man nd ausversehen ALLES zurückschickt
-
-        Gelöschte Nutzer nicht Authentifizieren: Gültige Tokens für nicht existierende Benutzer sind gefährlich!
-
-        Email Verifikation: an bestehende, unimplementierte Endpunkte in AuthController anknüpfen.
-        Email Schicken + Token Speichern (mit Ablauf @Scheduled)->Token Abgleichen und Löschen (wenn pw geändert)
 
         Profile: Entity getrennt von Benutzer, verbunden mit 1zu1 Beziehung, separate Endpunkte aber dennoch über
         Id des Users identifiziert -> Bio, Name, Events
 
         Medien: Object Storage, URLs in Entities speichern, ans Frontend nur temporär signierte URLs
         -> Avatare, Bilder für Veranstaltungen, Banner etc
+
+        Email-Auth: Email Addresse bestätigen bevor Login möglich
 
         Soft-Delete: Gelöschte Entitäten mit "boolean: deleted" o.Ä. flaggen + timestamp setzen um Wiederherstellung
         zu erlauben. Nach bestimmter Zeit dann löschen.
@@ -44,6 +37,11 @@ public class Eventbubble30Application {
         Moderation: Nutzer suspendieren, bannen, löschen. Beiträge melden. Moderator Ansicht für gemeldete Beiträge
 
         Captcha: Altcha o.Ä. (free und am besten self hosted)
+
+
+        -> später im Frontend Admin Dashboard
+        vlt kein Login erlauben wenn schon drin? (eig unnötig)
+        Gelöschte Nutzer nicht Authentifizieren: Gültige Tokens für nicht existierende Benutzer sind gefährlich!
 
         Event-Erinnerungen, Tags+Kategorien für Events, einfache Suche +Filter, Privatsphäreeinstellungen Profile+Events, Webhooks, Session Overview, MFA, Cache Layer, Telemetrie
      */
