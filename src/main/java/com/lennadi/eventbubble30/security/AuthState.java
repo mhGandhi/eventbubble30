f2e68cb477afb6dbc349baee699d6906c853bbd0
Adrian <adrian.akipi@web.de>
2025-12-06 14:15:08 +0100

    Anordnung geändert

--- src/main/java/com/lennadi/eventbubble30/security/AuthState.java
+++ src/main/java/com/lennadi/eventbubble30/security/AuthState.java
@@ -1,22 +1,22 @@
 package com.lennadi.eventbubble30.security;
 
 public enum AuthState {
+    //standard
     AUTHENTICATED,
     NO_TOKEN,
 
-    EMPTY_TOKEN,
-    MALFORMED,
-    WRONG_AUTH_FORMAT,
-
-    INVALID_SIGNATURE,
-
+    //abgelaufen
     EXPIRED,
     REVOKED,
     REVOKED_GLOBAL,
     PASSWORD_CHANGED,
 
+    //Fehler
+    EMPTY_TOKEN,
+    MALFORMED,
+    WRONG_AUTH_FORMAT,
+    INVALID_SIGNATURE,
     WRONG_TOKEN_TYPE,
-
     USER_NOT_FOUND,
 
     UNKNOWN
