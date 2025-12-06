80cbc224941709714bb950f1f6513a777825122f
Adrian <adrian.akipi@web.de>
2025-12-06 03:21:31 +0100

    import clean + anno für doFilter Methode

--- src/main/java/com/lennadi/eventbubble30/filter/LastSeenFilter.java
+++ src/main/java/com/lennadi/eventbubble30/filter/LastSeenFilter.java
@@ -1,22 +1,18 @@
 package com.lennadi.eventbubble30.filter;
 
-import com.lennadi.eventbubble30.CronJobs;
 import com.lennadi.eventbubble30.security.BenutzerDetails;
 import com.lennadi.eventbubble30.features.service.BenutzerService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.autoconfigure.health.HealthEndpointProperties;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 
@@ -28,7 +24,9 @@ public class LastSeenFilter extends OncePerRequestFilter {
     private final BenutzerService benutzerService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
             throws ServletException, IOException {
         try{
             var auth = SecurityContextHolder.getContext().getAuthentication();
