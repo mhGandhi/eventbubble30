e81da64d7003c192f97f885cbf9176a2f4620f11
Adrian <adrian.akipi@web.de>
2025-12-24 14:36:13 +0100

    Spalten umbenannt zu "location_[...]"

--- src/main/java/com/lennadi/eventbubble30/features/db/Location.java
+++ src/main/java/com/lennadi/eventbubble30/features/db/Location.java
@@ -1,22 +1,39 @@
 package com.lennadi.eventbubble30.features.db;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.Setter;
 
 @Embeddable
-@Getter @Setter
-public class Location {//todo entity vlt
+@Getter
+@Setter
+public class Location {
+
+    @Column(name = "location_display_name")
     private String displayName;
 
+    @Column(name = "location_latitude")
     private Double latitude;
+
+    @Column(name = "location_longitude")
     private Double longitude;
 
+    @Column(name = "location_street")
     private String street;
+
+    @Column(name = "location_city")
     private String city;
+
+    @Column(name = "location_postal_code")
     private String postalCode;
+
+    @Column(name = "location_country")
     private String country;
 
-    private String externalSource;//z.B. Nominatim
+    @Column(name = "location_external_source")
+    private String externalSource;
+
+    @Column(name = "location_external_id")
     private String externalId;
 }
