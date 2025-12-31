784e3c97abd33a028a71655c22b961dac7fbe890
Adrian <adrian.akipi@web.de>
2025-12-31 04:15:31 +0100

    event page fix

--- src/main/java/com/lennadi/eventbubble30/frontend/StaticPageController.java
+++ src/main/java/com/lennadi/eventbubble30/frontend/StaticPageController.java
@@ -5,8 +5,10 @@ import com.lennadi.eventbubble30.features.service.VeranstaltungService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -32,15 +34,27 @@ public class StaticPageController {
     }
 
     @GetMapping("/event")
-    public String event(
-            @RequestParam Long id,
+    public String eventIndex(Model model) {
+        model.addAttribute("event", null);
+        return "event";
+    }
+
+    @GetMapping("/event/{id}")
+    public String eventDetail(
+            @PathVariable Long id,
             Model model
     ) {
-        Veranstaltung ev = veranstaltungService.getVeranstaltungById(id);
+        Veranstaltung ev = veranstaltungService
+                .getVeranstaltungById(id);
+
+        if (ev == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
 
         model.addAttribute("event", ev);
         return "event";
     }
+
 
     @GetMapping("/profile")
     public String profile() {
