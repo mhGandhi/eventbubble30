f28282c80ceb5b991155863aed09c05478bceb5b
mhGandhi <adrian.akipi@web.de>
2026-01-16 02:47:40 +0100

    validate pp, catch nonexisting

--- src/main/java/com/lennadi/eventbubble30/fileStorage/FileManagerService.java
+++ src/main/java/com/lennadi/eventbubble30/fileStorage/FileManagerService.java
@@ -2,6 +2,7 @@ package com.lennadi.eventbubble30.fileStorage;
 
 import com.lennadi.eventbubble30.features.service.templates.ProfilePicture;
 import com.lennadi.eventbubble30.features.service.templates.StoredFile;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,6 +18,8 @@ public class FileManagerService {
     private final FileStorageService fileStorageService;
 
     public ProfilePicture toProfilePicture(MultipartFile file) throws IOException {
+        ProfilePicture.validateUpload(file.getContentType(), file.getSize());
+
         String newKey = fileStorageService.store(
                 file.getInputStream(),
                 file.getOriginalFilename(),
@@ -33,20 +36,25 @@ public class FileManagerService {
         return ret;
     }
 
-    public URL getURL(StoredFile storedFile, int ttlSec) {
+    public URL getURL(@NotNull StoredFile storedFile, int ttlSec) {
         Instant then =  Instant.now().plusSeconds(ttlSec);
         return getURL(storedFile, then);
     }
 
-    public URL getURL(StoredFile storedFile, Instant expiry) {
-        return fileStorageService.getFileURL(storedFile.getKey());//todo expiry
+    public URL getURL(@NotNull StoredFile storedFile, Instant expiry) {
+        try{
+            return fileStorageService.getFileURL(storedFile.getKey());//todo expiry
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public URL getURL(StoredFile storedFile) {
+    public URL getURL(@NotNull StoredFile storedFile) {
         return getURL(storedFile, null);
     }
 
-    public void delete(StoredFile file) {
+    public void delete(@NotNull StoredFile file) {
         fileStorageService.delete(file.getKey());
     }
 }
