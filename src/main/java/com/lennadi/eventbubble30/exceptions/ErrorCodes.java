080e4ad6ae6f1a1270db439ead8dacc6ec9bd616
mhGandhi <adrian.akipi@web.de>
2026-02-19 23:23:20 +0100

    ich werd verrückt. einfach flow auf den andern oder wie man das nennt amk
    error code implementation

---
+++ src/main/java/com/lennadi/eventbubble30/exceptions/ErrorCodes.java
@@ -0,0 +1,22 @@
+package com.lennadi.eventbubble30.exceptions;
+
+/**
+ * ja so für besseres feedback und so ja
+ */
+public enum ErrorCodes {
+
+    //401
+    INVALID_CREDENTIALS("E_INVALID_CREDENTIALS"),
+    EMAIL_NOT_VERIFIED("E_EMAIL_NOT_VERIFIED"),
+    ACCOUNT_NOT_ACTIVE("E_ACCOUNT_NOT_ACTIVE");
+
+    public final String actual;
+    ErrorCodes(String pActual){
+        actual = pActual;
+    }
+
+    @Override
+    public String toString() {
+        return actual;
+    }
+}
