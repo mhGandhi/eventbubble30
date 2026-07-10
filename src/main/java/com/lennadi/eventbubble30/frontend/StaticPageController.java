b1767e7e21b57dbd53c2bc62482e96a7749c0550
AA <abdulabsender@gmail.com>
2026-07-10 03:25:23 +0200

    Hase

--- src/main/java/com/lennadi/eventbubble30/frontend/StaticPageController.java
+++ src/main/java/com/lennadi/eventbubble30/frontend/StaticPageController.java
@@ -1,22 +1,30 @@
 package com.lennadi.eventbubble30.frontend;
 
 import com.lennadi.eventbubble30.features.db.entities.Veranstaltung;
+import com.lennadi.eventbubble30.features.service.TelegramNotifier;
 import com.lennadi.eventbubble30.features.service.VeranstaltungService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @Controller
 public class StaticPageController {
 
     private final VeranstaltungService veranstaltungService;
+    private final TelegramNotifier telegramNotifier;
 
     @GetMapping("/")
     public String index() {
@@ -37,6 +45,19 @@ public class StaticPageController {
     public String eventIndex(Model model) {
         model.addAttribute("event", null);
         return "event";
+    }
+
+    @GetMapping(value = "/hase_meme", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> haseBild() throws IOException {
+        telegramNotifier.send("HASE");
+
+        ClassPathResource imgFile = new ClassPathResource("static/images/hase.jpg");
+        byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(bytes);
     }
 
     @GetMapping("/event/{id}")
